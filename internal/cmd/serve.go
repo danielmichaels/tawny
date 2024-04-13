987aa519576a4f19c32818af287fcef1d6621f30
Daniel Michaels <dan@danielms.site>
2024-04-13 22:21:34 +1000

    Refactor goa into cobra command

--- internal/cmd/serve.go
+++ internal/cmd/serve.go
@@ -1,22 +1,108 @@
-package main
+package cmd
 
 import (
 	"context"
-	svclogger "github.com/danielmichaels/lappycloud/internal/logger"
-	"net/http"
-	"net/url"
-	"os"
-	"sync"
-	"time"
-
+	"fmt"
 	monitoringsvr "github.com/danielmichaels/lappycloud/gen/http/monitoring/server"
 	openapisvr "github.com/danielmichaels/lappycloud/gen/http/openapi/server"
-	monitoring "github.com/danielmichaels/lappycloud/gen/monitoring"
-	openapi "github.com/danielmichaels/lappycloud/gen/openapi"
+	"github.com/danielmichaels/lappycloud/gen/monitoring"
+	"github.com/danielmichaels/lappycloud/gen/openapi"
+	lappycloud "github.com/danielmichaels/lappycloud/internal/api"
+	svclogger "github.com/danielmichaels/lappycloud/internal/logger"
+	"github.com/spf13/cobra"
 	goahttp "goa.design/goa/v3/http"
 	httpmdlwr "goa.design/goa/v3/http/middleware"
 	"goa.design/goa/v3/middleware"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
+
+func ServeCmd(ctx context.Context) *cobra.Command {
+	var isConsole bool
+	var debugF bool
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Args:  cobra.ExactArgs(0),
+		Short: "Runs the servers",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var (
+				logger *svclogger.Logger
+			)
+			{
+				logger = svclogger.New("api", debugF, isConsole)
+			}
+
+			// Initialize the services.
+			var (
+				monitoringSvc monitoring.Service
+				openapiSvc    openapi.Service
+			)
+			{
+				monitoringSvc = lappycloud.NewMonitoring(logger)
+				openapiSvc = lappycloud.NewOpenapi(logger)
+			}
+
+			// Wrap the services in endpoints that can be invoked from other services
+			// potentially running in different processes.
+			var (
+				monitoringEndpoints *monitoring.Endpoints
+				openapiEndpoints    *openapi.Endpoints
+			)
+			{
+				monitoringEndpoints = monitoring.NewEndpoints(monitoringSvc)
+				openapiEndpoints = openapi.NewEndpoints(openapiSvc)
+			}
+
+			// Create channel used by both the signal handler and server goroutines
+			// to notify the main goroutine when to stop the server.
+			errc := make(chan error)
+
+			// Setup interrupt handler. This optional step configures the process so
+			// that SIGINT and SIGTERM signals cause the services to stop gracefully.
+			go func() {
+				c := make(chan os.Signal, 1)
+				signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+				errc <- fmt.Errorf("%s", <-c)
+			}()
+
+			var wg sync.WaitGroup
+			ctx, cancel := context.WithCancel(ctx)
+
+			addr := "http://localhost:9090"
+			u, err := url.Parse(addr)
+			if err != nil {
+				logger.Fatal().Msgf("invalid URL %#v: %s\n", addr, err)
+			}
+			handleHTTPServer(
+				ctx,
+				u,
+				monitoringEndpoints,
+				openapiEndpoints,
+				&wg,
+				errc,
+				logger,
+				debugF,
+			)
+			logger.Info().Msgf("exiting (%v)", <-errc)
+
+			// Send cancellation signal to the goroutines.
+			cancel()
+
+			wg.Wait()
+			logger.Info().Msg("exited")
+			return nil
+		},
+		//RunE: serve,
+	}
+	cmd.Flags().BoolVar(&debugF, "debug", false, "Log request and response bodies")
+	cmd.Flags().BoolVar(&isConsole, "console", false, "Use zerolog ConsoleWriter")
+	return cmd
+}
 
 // handleHTTPServer starts configures and starts a HTTP server on the given
 // URL. It shuts down the server if any error is received in the error channel.
