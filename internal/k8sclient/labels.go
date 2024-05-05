5c605f3e0cc142291735519e03b79e7c1fd5a2de
Daniel Michaels <dan@danielms.site>
2024-05-06 00:26:37 +1000

    Add Kubernetes label creation functionality
    Added a new file, labels.go, in the internal/k8sclient directory. This file defines a function, CreateLabels, which generates labels for Kubernetes objects. Also added label options for custom labels, component, name, and a core option.

---
+++ internal/k8sclient/labels.go
@@ -0,0 +1,68 @@
+package k8sclient
+
+import (
+	"fmt"
+
+	assets "github.com/danielmichaels/tawny"
+)
+
+type LabelOpts struct {
+	Name      string
+	Component string
+	Extra     map[string]string
+	Core      bool
+}
+
+type LabelOpt func(*LabelOpts)
+
+func WithName(name string) LabelOpt {
+	return func(args *LabelOpts) {
+		args.Name = name
+	}
+}
+
+func WithComponent(component string) LabelOpt {
+	return func(args *LabelOpts) {
+		args.Component = component
+	}
+}
+
+func WithCustomLabel(key, value string) LabelOpt {
+	return func(args *LabelOpts) {
+		if args.Extra == nil {
+			args.Extra = make(map[string]string)
+		}
+		args.Extra[key] = value
+	}
+}
+func WithCoreLabel(core bool) LabelOpt {
+	return func(l *LabelOpts) {
+		l.Core = core
+	}
+}
+
+func CreateLabels(opts ...LabelOpt) map[string]string {
+	args := &LabelOpts{}
+
+	for _, opt := range opts {
+		opt(args)
+	}
+
+	labels := map[string]string{
+		"tawny.sh/name":       fmt.Sprintf("%s-%s", assets.AppName, args.Name),
+		"tawny.sh/component":  fmt.Sprintf("%s-%s", assets.AppName, args.Component),
+		"tawny.sh/part-of":    fmt.Sprintf("%s", assets.AppName),
+		"tawny.sh/managed-by": fmt.Sprintf("%s", assets.AppName),
+	}
+	if args.Core {
+		for k, v := range labels {
+			labels[k] = fmt.Sprintf("%s-core", v)
+		}
+	}
+
+	for k, v := range args.Extra {
+		labels[k] = v
+	}
+
+	return labels
+}
