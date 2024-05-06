48bf46e88d129e92d633fbea86d2e1db94f220ff
Daniel Michaels <dan@danielms.site>
2024-05-06 15:11:38 +1000

    golines cleanup

--- internal/k8sclient/services.go
+++ internal/k8sclient/services.go
@@ -2,9 +2,16 @@ package k8sclient
 
 import (
 	"context"
+	"fmt"
+
+	assets "github.com/danielmichaels/tawny"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func serviceNameGenerator(name string) string {
+	return fmt.Sprintf(DefaultServiceName, assets.AppName, name)
+}
 
 func (k K8sClient) ListServices(ctx context.Context, namespace string) (*v1.ServiceList, error) {
 	res, err := k.Client.CoreV1().Services(namespace).List(ctx, metav1.ListOptions{})
